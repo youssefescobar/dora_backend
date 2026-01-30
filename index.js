10f4bb9c43aca848df936976cdf3aaf3eb930d62
youssefescobar <coolgoot123@gmail.com>
2026-01-30 19:42:17 +0200

    up

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const admin_routes = require('./routes/admin_routes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Use the winston http logger
